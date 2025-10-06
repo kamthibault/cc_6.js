76212dc3eb83d04a08c2ed000828f988127c497f
Kameron Thibault <kameronthibault@usf.edu>
2025-10-05 20:15:18 -0400

    Step 2 & 3

--- cc_6.js
+++ cc_6.js
@@ -5,3 +5,26 @@ class Employee {
 
     }
 }
+const Employee = {
+describe () {
+return `${this.name} works in ${this.department}`;
+
+
+}
+};
+
+const emp1 = new Employee ("Juan", "Accounting");
+const emp2 = new Employee ("Jason", "Analytics");
+const emp3 = new Employee ("Jorge", "Sales")
+
+class Manager extends Employee {
+    constructor (teamSize, name, department){
+    super(name, department)
+        this.teamSizec = teamSize
+    }
+}
+
+const man1 = new Manager (15, "George", "Accounting")
+const man2 = new Manager (26, "Will", "Sales")
+const man3 = new Manager (12, "Harry", "Analytics")
+
