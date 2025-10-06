24a267b3bad66758298af2dedc7f4fddbc71787a
Kameron Thibault <kameronthibault@usf.edu>
2025-10-05 20:28:07 -0400

    Progress

--- cc_6.js
+++ cc_6.js
@@ -4,14 +4,19 @@ class Employee {
         this.department = department;
 
     }
-}
-const Employee = {
+};
+const employee = {
 describe () {
 return `${this.name} works in ${this.department}`;
 
 
 }
 };
+const James = Object.create (employee);
+James.name = "James"
+James.department = "Marketing"
+
+console.log (James.describe());
 
 const emp1 = new Employee ("Juan", "Accounting");
 const emp2 = new Employee ("Jason", "Analytics");
@@ -23,6 +28,19 @@ class Manager extends Employee {
         this.teamSizec = teamSize
     }
 }
+const manager = {
+describe () {
+return `${this.name} manages ${this.department}`;
+
+
+}
+};
+
+const Nomar = Object.create (manager);
+Nomar.name = "Nomar"
+Nomar.department = "Production"
+
+console.log (Nomar.describe())
 
 const man1 = new Manager (15, "George", "Accounting")
 const man2 = new Manager (26, "Will", "Sales")
